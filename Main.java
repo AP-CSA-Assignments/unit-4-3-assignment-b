0a21fd17fd0dc9b652aa63e8ca8fe9afb4da8a9b
Kyle Vu <ktvu@hbuhsd.edu>
2024-11-07 20:29:11 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,14 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    // You can test your method here
+    // System.out.println(isPrime(1));
+    // System.out.println(isPrime(17));
+  }
+
+  public static boolean isPrime(int N)
+  {
+    // TODO: Replace and write your code below
+    return false;
   }
 }
